15106a87fda1f6f40563aca8f73e21a4cd4abd6e
Nicolas Bordenave <nborde@gmail.com>
2015-12-10 20:33:54 -0300

    add local mac con string

--- Server.js
+++ Server.js
@@ -36,8 +36,9 @@ REST.prototype.connectMysql = function () {
 
 REST.prototype.connectPostgres = function () {
     var self = this;
+    var conString = "postgres://postgres:admin@localhost/estudio_legal";
     //var conString = "postgres://postgres:''@localhost/d1c9jmonqhru2t";
-    var conString = "postgres://jcdahlngnwpxgo:1EXHw_a_pIDWUCm1slcjQel6m7@ec2-54-83-59-203.compute-1.amazonaws.com:5432/dad25cup395vqk";
+    //var conString = "postgres://jcdahlngnwpxgo:1EXHw_a_pIDWUCm1slcjQel6m7@ec2-54-83-59-203.compute-1.amazonaws.com:5432/dad25cup395vqk";
 
    var pool =  pg.connect((process.env.DATABASE_URL || conString), function(err, client, done) {
        if (err) {
